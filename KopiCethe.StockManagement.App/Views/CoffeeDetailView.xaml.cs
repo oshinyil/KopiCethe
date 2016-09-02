86c55f010ba0c0634e6958ea6e64183c1b7e97e8
oshinyil <taneyoroshi@gmail.com>
2016-09-03 05:23:34 +0800

    Implemented single object data binding

--- KopiCethe.StockManagement.App/Views/CoffeeDetailView.xaml.cs
+++ KopiCethe.StockManagement.App/Views/CoffeeDetailView.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using KopiCethe.StockManagement.Model;
 using MahApps.Metro.Controls;
-using System;
 using System.Windows;
-using System.Windows.Media.Imaging;
 
 namespace KopiCethe.StockManagement.App.Views
 {
@@ -22,27 +20,7 @@ namespace KopiCethe.StockManagement.App.Views
 
         void CoffeeDetailView_Loaded(object sender, RoutedEventArgs e)
         {
-            LoadData();
-        }
-
-        private void LoadData()
-        {
-            CoffeeNameLabel.Content = SelectedCoffee.Name;
-            CoffeeIdTextBox.Text = SelectedCoffee.Id.ToString();
-            CoffeeDescriptionTextBox.Text = SelectedCoffee.Description;
-            CoffeePriceTextBox.Text = SelectedCoffee.Price.ToString();
-            StockAmountTextBox.Text = SelectedCoffee.AmountInStock.ToString();
-            FirstTimeAddedTextBox.Text = SelectedCoffee.FirstAddedToStockDate.ToShortDateString();
-            if (SelectedCoffee is SuperiorCoffee)
-                ExtraDescriptionTextBox.Text = (SelectedCoffee as SuperiorCoffee).ExtraDescription;
-            else
-                ExtraDescriptionTextBox.Text = "NA";
-
-            BitmapImage img = new BitmapImage();
-            img.BeginInit();
-            img.UriSource = new Uri("/KopiCethe.StockManagement.App;component/Images/coffee" + SelectedCoffee.Id + ".jpg", UriKind.Relative);
-            img.EndInit();
-            CoffeeImage.Source = img;
+            this.DataContext = SelectedCoffee;
         }
 
         private void DeleteCoffeeButton_Click(object sender, RoutedEventArgs e)
