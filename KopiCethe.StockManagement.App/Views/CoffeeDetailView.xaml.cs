e3e2ef0ad21a3f496721aa0194931d32da78bfb6
oshinyil <taneyoroshi@gmail.com>
2016-09-03 05:13:09 +0800

    Added views.

---
+++ KopiCethe.StockManagement.App/Views/CoffeeDetailView.xaml.cs
@@ -0,0 +1,58 @@
+﻿using KopiCethe.StockManagement.Model;
+using MahApps.Metro.Controls;
+using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace KopiCethe.StockManagement.App.Views
+{
+    /// <summary>
+    /// Interaction logic for CoffeeDetailView.xaml
+    /// </summary>
+    public partial class CoffeeDetailView : MetroWindow
+    {
+        public Coffee SelectedCoffee { get; set; }
+
+        public CoffeeDetailView()
+        {
+            InitializeComponent();
+
+            this.Loaded += CoffeeDetailView_Loaded;
+        }
+
+        void CoffeeDetailView_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            CoffeeNameLabel.Content = SelectedCoffee.Name;
+            CoffeeIdTextBox.Text = SelectedCoffee.Id.ToString();
+            CoffeeDescriptionTextBox.Text = SelectedCoffee.Description;
+            CoffeePriceTextBox.Text = SelectedCoffee.Price.ToString();
+            StockAmountTextBox.Text = SelectedCoffee.AmountInStock.ToString();
+            FirstTimeAddedTextBox.Text = SelectedCoffee.FirstAddedToStockDate.ToShortDateString();
+            if (SelectedCoffee is SuperiorCoffee)
+                ExtraDescriptionTextBox.Text = (SelectedCoffee as SuperiorCoffee).ExtraDescription;
+            else
+                ExtraDescriptionTextBox.Text = "NA";
+
+            BitmapImage img = new BitmapImage();
+            img.BeginInit();
+            img.UriSource = new Uri("/KopiCethe.StockManagement.App;component/Images/coffee" + SelectedCoffee.Id + ".jpg", UriKind.Relative);
+            img.EndInit();
+            CoffeeImage.Source = img;
+        }
+
+        private void DeleteCoffeeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void SaveCoffeeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ KopiCethe.StockManagement.App/Views/CoffeeOverviewView.xaml.cs
@@ -0,0 +1,64 @@
+﻿using KopiCethe.StockManagement.App.Services;
+using KopiCethe.StockManagement.Model;
+using MahApps.Metro.Controls;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace KopiCethe.StockManagement.App.Views
+{
+    /// <summary>
+    /// Interaction logic for CoffeeOverviewView.xaml
+    /// </summary>
+    public partial class CoffeeOverviewView : MetroWindow
+    {
+        private Coffee selectedCoffee;
+
+        public CoffeeOverviewView()
+        {
+            InitializeComponent();
+
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            var coffeeDataService = new CoffeeDataService();
+            CoffeeListView.ItemsSource = coffeeDataService.GetAllCoffees();
+        }
+
+        private void EditCoffeeButton_Click(object sender, RoutedEventArgs e)
+        {
+            CoffeeDetailView coffeeDetailView = new CoffeeDetailView();
+            coffeeDetailView.SelectedCoffee = selectedCoffee;
+            coffeeDetailView.ShowDialog();
+        }
+
+        private void CoffeeListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            selectedCoffee = e.AddedItems[0] as Coffee;
+
+            if (e != null)
+            {
+                CoffeeIdLabel.Content = selectedCoffee.Id;
+                CoffeeNameLabel.Content = selectedCoffee.Name;
+                DescriptionLabel.Content = selectedCoffee.Description;
+                PriceLabel.Content = selectedCoffee.Price;
+                StockAmountLabel.Content = selectedCoffee.AmountInStock.ToString();
+                FirstTimeAddedLabel.Content = selectedCoffee.FirstAddedToStockDate.ToShortDateString();
+
+                BitmapImage img = new BitmapImage();
+                img.BeginInit();
+                img.UriSource = new Uri("/KopiCethe.StockManagement.App;component/Images/coffee" + selectedCoffee.Id + ".jpg", UriKind.Relative);
+                img.EndInit();
+                CoffeeImage.Source = img;
+            }
+        }
+
+        private void AddFakeCoffeeButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
