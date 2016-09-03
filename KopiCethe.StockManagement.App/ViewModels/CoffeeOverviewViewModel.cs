8bdf3a644a5aa5e0513fdde780e61243d3934e90
oshinyil <taneyoroshi@gmail.com>
2016-09-03 08:38:30 +0800

    Refactored CoffeeOverview to MVVM

---
+++ KopiCethe.StockManagement.App/ViewModels/CoffeeOverviewViewModel.cs
@@ -0,0 +1,62 @@
+﻿using KopiCethe.StockManagement.App.Extensions;
+using KopiCethe.StockManagement.App.Services;
+using KopiCethe.StockManagement.Model;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace KopiCethe.StockManagement.App.ViewModels
+{
+    public class CoffeeOverviewViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private CoffeeDataService coffeeDataService;
+
+        private ObservableCollection<Coffee> coffees;
+        public ObservableCollection<Coffee> Coffees
+        {
+            get
+            {
+                return coffees;
+            }
+            set
+            {
+                coffees = value;
+                RaisePropertyChanged("Coffees");
+            }
+        }
+
+        private Coffee selectedCoffee;
+
+        public Coffee SelectedCoffee
+        {
+            get
+            {
+                return selectedCoffee;
+            }
+            set
+            {
+                selectedCoffee = value;
+                RaisePropertyChanged("SelectedCoffee");
+            }
+        }
+
+        public CoffeeOverviewViewModel()
+        {
+            coffeeDataService = new CoffeeDataService();
+            LoadData();
+        }
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private void LoadData()
+        {
+            Coffees = coffeeDataService.GetAllCoffees().ToObservableCollection();
+        }
+    }
+}
--- KopiCethe.StockManagement.App/Views/CoffeeOverviewView.xaml.cs
+++ KopiCethe.StockManagement.App/Views/CoffeeOverviewView.xaml.cs
@@ -1,13 +1,4 @@
-﻿using KopiCethe.StockManagement.App.Services;
-using KopiCethe.StockManagement.App.Extensions;
-using KopiCethe.StockManagement.Model;
-using MahApps.Metro.Controls;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Media.Imaging;
+﻿using MahApps.Metro.Controls;
 
 namespace KopiCethe.StockManagement.App.Views
 {
@@ -16,65 +7,9 @@ namespace KopiCethe.StockManagement.App.Views
     /// </summary>
     public partial class CoffeeOverviewView : MetroWindow
     {
-        private Coffee selectedCoffee;
-        private ObservableCollection<Coffee> coffees;
         public CoffeeOverviewView()
         {
             InitializeComponent();
-
-            LoadData();
-        }
-
-        private void LoadData()
-        {
-            var coffeeDataService = new CoffeeDataService();
-            coffees = coffeeDataService.GetAllCoffees().ToObservableCollection();
-            CoffeeListView.ItemsSource = coffees;
-        }
-
-        private void EditCoffeeButton_Click(object sender, RoutedEventArgs e)
-        {
-            CoffeeDetailView coffeeDetailView = new CoffeeDetailView();
-            coffeeDetailView.SelectedCoffee = selectedCoffee;
-            coffeeDetailView.ShowDialog();
-        }
-
-        private void CoffeeListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            selectedCoffee = e.AddedItems[0] as Coffee;
-
-            if (e != null)
-            {
-                CoffeeIdLabel.Content = selectedCoffee.Id;
-                CoffeeNameLabel.Content = selectedCoffee.Name;
-                DescriptionLabel.Content = selectedCoffee.Description;
-                PriceLabel.Content = selectedCoffee.Price;
-                StockAmountLabel.Content = selectedCoffee.AmountInStock.ToString();
-                FirstTimeAddedLabel.Content = selectedCoffee.FirstAddedToStockDate.ToShortDateString();
-
-                BitmapImage img = new BitmapImage();
-                img.BeginInit();
-                img.UriSource = new Uri("/KopiCethe.StockManagement.App;component/Images/coffee" + selectedCoffee.Id + ".jpg", UriKind.Relative);
-                img.EndInit();
-                CoffeeImage.Source = img;
-            }
-        }
-
-        private void AddFakeCoffeeButton_Click(object sender, RoutedEventArgs e)
-        {
-            Coffee coffee = new Coffee()
-            {
-                Id = 123,
-                Name = "Test coffee",
-                Description = "Simply the best coffee",
-                ImageId = 1,
-                AmountInStock = 1000,
-                InStock = true,
-                FirstAddedToStockDate = new DateTime(2014, 1, 3),
-                OriginCountry = Country.Ethiopia,
-                Price = 12
-            };
-            coffees.Add(coffee);
         }
     }
 }
