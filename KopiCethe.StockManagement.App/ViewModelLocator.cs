b267a25d2ff823753937c31f62eedc4d9d82ca41
oshinyil <taneyoroshi@gmail.com>
2016-09-03 09:01:41 +0800

    Added ViewModelLocator.

---
+++ KopiCethe.StockManagement.App/ViewModelLocator.cs
@@ -0,0 +1,26 @@
+﻿using KopiCethe.StockManagement.App.ViewModels;
+
+namespace KopiCethe.StockManagement.App
+{
+    public class ViewModelLocator
+    {
+        private static CoffeeOverviewViewModel coffeeOverviewViewModel = new CoffeeOverviewViewModel();
+        private static CoffeeDetailViewModel coffeeDetailViewModel = new CoffeeDetailViewModel();
+
+        public static CoffeeDetailViewModel CoffeeDetailViewModel
+        {
+            get
+            {
+                return coffeeDetailViewModel;
+            }
+        }
+
+        public static CoffeeOverviewViewModel CoffeeOverviewViewModel
+        {
+            get
+            {
+                return coffeeOverviewViewModel;
+            }
+        }
+    }
+}
